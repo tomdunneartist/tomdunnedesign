daa2399b842dac25454abe359c0012ad496f58d8
Tom Dunne <tomdunneartist@gmail.com>
2025-02-11 10:00:29 -0500

    add bluesky link

--- script.js
+++ script.js
@@ -98,7 +98,7 @@ link2.onclick = function () {
     window.open("https://www.tomdunne.dev", '_blank');
 }
 link3.onclick = function () {
-    window.open("https://www.linkedin.com/in/tomdunnedesign", '_blank');
+    window.open("https://bsky.app/profile/tomdunnedev.bsky.social", '_blank');
 }
 
 
